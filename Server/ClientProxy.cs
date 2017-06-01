91d1d7681352131292a345cbf9b5c5b5bab9365c
MartinesCarlo <MartinesCarlo@U12WKS10>
2017-06-01 15:31:55 +0200

    Server comit 1

---
+++ Server/ClientProxy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class ClientProxy
+    {
+        private Socket socket = null;
+
+        public ClientProxy(Socket s)
+        {
+            this.socket = s;
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,83 @@
+﻿using Server;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Server
+{
+    public class Server
+    {
+        private const int serverListenPort = 10000;
+        private const int sleepTime = 200;
+        private IPAddress ipAddress = IPAddress.Any;
+        private Thread mainThread;
+        private List<ClientProxy> vieleClients;
+        private Socket socket = null;
+        private OleDbConnection connection;
+        private OleDbDataAdapter adapter;
+        private DataSet ds;
+
+
+        public Server()
+        {
+            vieleClients = new List<ClientProxy>();
+            mainThread = new Thread(new ThreadStart(this.mainListener));
+            mainThread.Start();
+            ladeDatenbank();
+            Console.ReadLine();
+
+
+        }
+
+        private void ladeDatenbank()
+        {
+            connection = new OleDbConnection(Properties.Settings.Default.db1);
+            adapter = new OleDbDataAdapter
+            
+        }
+
+        private void mainListener()
+        {
+            TcpListener listener = new TcpListener(ipAddress, serverListenPort);
+            System.Console.WriteLine("Listening on port " + serverListenPort + "...");
+            while (true)
+            {
+                try
+                {
+                listener.Start();
+                
+                    while (!listener.Pending()) { Thread.Sleep(sleepTime); }
+                    socket = listener.AcceptSocket();
+                    vieleClients.Add(new ClientProxy(socket));
+                    System.Console.WriteLine("Neue Client-Verbindung (" +
+                                "IP: " + socket.RemoteEndPoint + ", Port: " + ((IPEndPoint)socket.LocalEndPoint).Port.ToString() + ")");
+
+                
+
+                }
+                catch(ThreadInterruptedException)
+                {
+                    Console.WriteLine("listener wird beendet");
+                    return;
+
+                }
+                catch (Exception ex)
+                {
+                    mainThread.Interrupt();                    
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Server();
+        }
+    }
+}
